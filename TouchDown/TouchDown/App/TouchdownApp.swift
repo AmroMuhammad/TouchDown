643734f7aa79da0f27fdd72af3a8c17bf5e73b9a
Amr Muhammad <amrmu94@gmail.com>
2023-03-06 14:16:42 +0200

    init commit

---
+++ TouchDown/TouchDown/App/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Amr Muhammad on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TouchDown/TouchDown/Utility/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Amr Muhammad on 06/03/2023.
+//
+
+import SwiftUI
+
+// COLOR
+let colorBackground = Color("ColorBackground")
+let colorGray = Color(UIColor.systemGray4)
